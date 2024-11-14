eb6cc6bd0f63600799d8950fece7a3fa0460ff13
GrimaldiDamian <grimaldidamian001@gmail.com>
2024-11-14 19:11:31 -0300

    Mejoras en el login, para que cree el usuario o para logearse

--- cliente Api/src/login/login.py
+++ cliente Api/src/login/login.py
@@ -3,9 +3,18 @@ from src.utilidades.utilidades import *
 import requests
 
 class login():
+    """
+    Clase que se encarga de manejar el login del usuario y la creación de la cuenta
+    Args:
+        logeo (str): Es el nombre de usuario
+        password (str): Es la contraseña del usuario
+        correo (str): Es el correo del usuario
+        momento (str): Es el momento en el que se encuentra el usuario
+    """
     def __init__(self):
         self.logeo = ""
         self.password = ""
+        self.correo = ""
         self.momento = "Login"
         self.img = pygame.image.load("./assets/img/login.png") #cargar la imagen correspondiente
 
@@ -33,28 +42,44 @@ class login():
         if event.type == pygame.TEXTINPUT:
             if self.momento == "Login":
                 self.logeo += event.text
-            else:
+            elif self.momento == "Password":
                 self.password += event.text
+            else:
+                self.correo += event.text
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_BACKSPACE:
                 if self.momento == "Login":
                     self.logeo = self.logeo[:-1]
-                else:
+                elif self.momento == "Password":
                     self.password = self.password[:-1]
+                else:
+                    self.correo = self.correo[:-1]
             elif event.key == pygame.K_RETURN:
                 if self.momento == "Login":
                     self.momento = "Password"
                 else:
-                    self.login()
+                    if global_variables.etapa == "login":
+                        self.login()
+                    else:
+                        self.creacion_cuenta()
+            elif event.key == pygame.K_TAB:
+                if self.momento == "Login":
+                    self.momento = "Password"
+                elif self.momento == "Password":
+                    if global_variables.etapa == "login":
+                        self.momento = "Login"
+                    else:
+                        self.momento = "Correo"
+                else:
+                    self.momento = "Login"
 
-    def dibujar(self,screen : pygame.Surface,fuente : pygame.font):
+    def dibujar_inicio(self,screen : pygame.Surface,fuente : pygame.font):
         """
-        Dibuja en pantalla el login
+        Dibuja en pantalla el inicio del login
         Args:
             screen (pygame.Surface): Es la pantalla en la que se dibujara
             fuente (pygame.font): Es la fuente que se utilizara para escribir en pantalla
         """
-        screen.blit(self.img, (0,0))
         x = self.centrar_texto(texto = 'Ingrese el usuario',fuente = fuente)
         y = 0
         self.dibujar_texto(screen,'Ingrese el usuario',fuente,x,y)
@@ -68,6 +93,34 @@ class login():
         x = self.centrar_texto(self.password,fuente)
         self.dibujar_texto(screen,self.password,fuente,x,y)
 
+    def dibujar_creacion(self,screen : pygame.Surface,fuente : pygame.font):
+        """
+        Dibuja en pantalla la creacion de la cuenta
+        Args:
+            screen (pygame.Surface): Es la pantalla en la que se dibujara
+            fuente (pygame.font): Es la fuente que se utilizara para escribir en pantalla
+        """
+        self.dibujar_inicio(screen,fuente)
+        x = self.centrar_texto('Ingrese correo electronico',fuente)
+        y = 3*tamaño_letra
+        self.dibujar_texto(screen,'Ingrese correo electronico',fuente,x,y)
+        y+= tamaño_letra
+        x = self.centrar_texto(self.correo,fuente)
+        self.dibujar_texto(screen,self.correo,fuente,x,y)
+
+    def dibujar(self,screen : pygame.Surface,fuente : pygame.font):
+        """
+        Dibuja en pantalla el login
+        Args:
+            screen (pygame.Surface): Es la pantalla en la que se dibujara
+            fuente (pygame.font): Es la fuente que se utilizara para escribir en pantalla
+        """
+        screen.blit(self.img, (0,0))
+        if global_variables.etapa == "login":
+            self.dibujar_inicio(screen,fuente)
+        elif global_variables.etapa == "creacion":
+            self.dibujar_creacion(screen,fuente)
+
     def resetear(self):
         """
         Resetea los valores de login y password
@@ -76,17 +129,34 @@ class login():
         self.password = ""
         self.momento = "Login"
 
+    def creacion_cuenta(self):
+        """
+        Realiza el proceso de creación de la cuenta
+        """
+        try:
+            data = {
+                "username": self.logeo,
+                "password": self.password,
+                "email": self.correo
+            }
+            response = requests.post(f"{url}/usuarios/Crear cuenta", data=data)
+            print(response.json())
+            self.resetear()
+            global_variables.etapa = "login"
+        except:
+            self.resetear()
+            print("Account creation failed. Please try again.")
+
     def login(self):
         """
         Realiza el proceso de login
         """
-        url = "http://localhost:8000/token"
         try:
             data = {
                 "username": self.logeo,
                 "password": self.password
             }
-            response = requests.post(url, data=data)
+            response = requests.post(f"{url}/token", data=data)
             global_variables.token = response.json()
             self.resetear()
             global_variables.etapa = "menu"
--- cliente Api/src/utilidades/utilidades.py
+++ cliente Api/src/utilidades/utilidades.py
@@ -2,6 +2,8 @@ ancho = 800
 alto = 600
 tamaño_letra = 25
 
+url = "http://localhost:8000"
+
 class variables_globales():
     def __init__(self) -> None:
         self.etapa = "login"
